ca6b367c90bc2ee8ed9e0f2cd8b8751f1f467e36
Damon McMinn <damonrmcminn@gmail.com>
2015-04-18 08:18:17 +0100

    More types

--- es6/typecheck.js
+++ es6/typecheck.js
@@ -6,3 +6,27 @@ export function isNumber(n) {
   return Number.isFinite(n);
 
 }
+
+export function isString(str) {
+
+  return typeof str === 'string';
+
+}
+
+export function isBool(bool) {
+
+  return typeof bool === 'boolean';
+
+}
+
+export function isRegex(regex) {
+
+  return regex instanceof RegExp;
+
+}
+
+export function isDate(date) {
+
+  return date instanceof Date;
+
+}
--- spec/typecheckSpec.js
+++ spec/typecheckSpec.js
@@ -3,7 +3,7 @@
 const tc = require('../index');
 const fn = function() {};
 
-describe('Number', function() {
+describe('isNumber', function() {
 
   it('should return true if input is int, float, positive or negative', function() {
 
@@ -41,6 +41,132 @@ describe('Number', function() {
     pass.forEach(function(p) {
       expect(p).toBe(true);
     });
+
+  });
+
+});
+
+describe('isString', function() {
+
+  it('should return true if a string', function() {
+
+    let n = tc.isString;
+
+    let fail = [
+      n([]),
+      n({}),
+      n(1),
+      n(fn),
+      n(new Date()),
+      n(/regex/),
+      n(true),
+      n(null),
+      n(undefined),
+      n(NaN),
+      n(Infinity),
+      n(-Infinity)
+    ];
+
+    fail.forEach(function(f) {
+      expect(f).toBe(false);
+    });
+
+    expect(n('string')).toBe(true);
+
+  });
+
+});
+
+describe('isBool', function() {
+
+  it('should return true if a boolean', function() {
+
+    let n = tc.isBool;
+
+    let fail = [
+      n([]),
+      n({}),
+      n(0),
+      n(fn),
+      n(new Date()),
+      n(/regex/),
+      n('str'),
+      n(null),
+      n(undefined),
+      n(NaN),
+      n(Infinity),
+      n(-Infinity)
+    ];
+
+    fail.forEach(function(f) {
+      expect(f).toBe(false);
+    });
+
+    expect(n(true)).toBe(true);
+    expect(n(false)).toBe(true);
+
+  });
+
+});
+
+describe('isRegex', function() {
+
+  it('should return true if a regex', function() {
+
+    let n = tc.isRegex;
+
+    let fail = [
+      n([]),
+      n({}),
+      n(1),
+      n(fn),
+      n(new Date()),
+      n(true),
+      n('str'),
+      n(null),
+      n(undefined),
+      n(NaN),
+      n(Infinity),
+      n(-Infinity)
+    ];
+
+    fail.forEach(function(f) {
+      expect(f).toBe(false);
+    });
+
+    expect(n(/regex/)).toBe(true);
+    expect(n(new RegExp('str'))).toBe(true);
+
+  });
+
+});
+
+describe('isDate', function() {
+
+  it('should return true if a Date object', function() {
+
+    let n = tc.isDate;
+
+    let fail = [
+      n([]),
+      n({}),
+      n(1),
+      n(fn),
+      n(/regex/),
+      n(true),
+      n('str'),
+      n(null),
+      n(undefined),
+      n(NaN),
+      n(Infinity),
+      n(-Infinity)
+    ];
+
+    fail.forEach(function(f) {
+      expect(f).toBe(false);
+    });
+
+    expect(n(new Date())).toBe(true);
 
   });
 
